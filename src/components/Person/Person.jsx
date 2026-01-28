083524b0c13cbab3d9fa0141a9ee790a4469014b
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2026-01-28 15:21:13 +0100

    refactor: refact function to variable

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,18 +1,14 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  function checkSpouse() {
-    return sex === 'm' ? 'wife' : 'husband';
-  }
+  const spouse = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age > 0 && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">
-        {isMarried
-          ? `${partnerName} is my ${checkSpouse()}`
-          : 'I am not married'}
+        {isMarried ? `${partnerName} is my ${spouse}` : 'I am not married'}
       </p>
     </section>
   );
